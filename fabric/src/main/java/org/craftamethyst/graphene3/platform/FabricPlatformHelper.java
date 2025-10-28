a98760b09356a27373dc2e8abb0993c465865b9a
QianMo0721 <3142150691@qq.com>
2025-10-29 00:42:54 +0800

    Multiloader init (1.20)

---
+++ fabric/src/main/java/org/craftamethyst/graphene3/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package org.craftamethyst.graphene3.platform;
+
+import org.craftamethyst.graphene3.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}

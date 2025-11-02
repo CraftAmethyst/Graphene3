1fa7e29d68289f9ab2267e032689bbb47de11c65
ZCRAFT <1149461164@qq.com>
2025-11-02 19:19:28 +0800

    perf: add cofig
    [skip ci]

--- common/src/main/java/org/craftamethyst/tritium/mixin/client/button/AbstractWidgetMixin.java
+++ common/src/main/java/org/craftamethyst/tritium/mixin/client/button/AbstractWidgetMixin.java
@@ -2,6 +2,7 @@ package org.craftamethyst.tritium.mixin.client.button;
 
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.EditBox;
+import org.craftamethyst.tritium.platform.Services;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,6 +20,9 @@ public abstract class AbstractWidgetMixin {
 
     @Inject(method = "mouseClicked", at = @At("RETURN"))
     public void onMouseClicked(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        if (!Services.CONFIG.get().fixes.buttonfix) {
+            return;
+        }
         AbstractWidget self = (AbstractWidget) (Object) this;
         if (self instanceof EditBox) {
             return;
@@ -30,6 +34,9 @@ public abstract class AbstractWidgetMixin {
 
     @Inject(method = "mouseReleased", at = @At("RETURN"))
     public void onMouseReleased(double mouseX, double mouseY, int button, CallbackInfoReturnable<Boolean> cir) {
+        if (!Services.CONFIG.get().fixes.buttonfix) {
+            return;
+        }
         AbstractWidget self = (AbstractWidget) (Object) this;
         if (self instanceof EditBox) {
             return;

5812a94f334de4ea22311a35c3ad0e19942551d6
QianMo0721 <3142150691@qq.com>
2025-11-02 18:29:19 +0800

    chore(config): Make NoiseSamplingCache configurable

--- common/src/main/java/org/craftamethyst/tritium/mixin/chunk/NoiseSamplingCacheMixin.java
+++ common/src/main/java/org/craftamethyst/tritium/mixin/chunk/NoiseSamplingCacheMixin.java
@@ -2,6 +2,7 @@ package org.craftamethyst.tritium.mixin.chunk;
 
 import net.minecraft.world.level.levelgen.DensityFunction;
 import net.minecraft.world.level.levelgen.NoiseChunk;
+import org.craftamethyst.tritium.platform.Services;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -25,6 +26,9 @@ public class NoiseSamplingCacheMixin {
 
     @Inject(method = "initializeForFirstCellX", at = @At("HEAD"))
     private void onFirstCell(CallbackInfo ci) {
+        if (!Services.CONFIG.get().serverPerformance.noiseSamplingCache) {
+            return;
+        }
         int startBlockX = net.minecraft.core.QuartPos.toBlock(firstNoiseX);
         int startBlockZ = net.minecraft.core.QuartPos.toBlock(firstNoiseZ);
         graphene$reusableContext.set(new DensityFunction.SinglePointContext(startBlockX, 0, startBlockZ));

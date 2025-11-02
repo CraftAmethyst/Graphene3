357c9ad6e282c36e8adf121701320e91472c6ce1
ZCRAFT <1149461164@qq.com>
2025-11-02 18:09:07 +0800

    drop forge(doge

--- forge/src/main/java/org/craftamethyst/tritium/config/TritiumConfig.java
+++
@@ -1,8 +0,0 @@
-package org.craftamethyst.tritium.config;
-
-import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.ConfigData;
-
-@Config(name = "tritium")
-public class TritiumConfig extends TritiumConfigBase implements ConfigData {
-}

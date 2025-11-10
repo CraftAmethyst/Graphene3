fee04c2e2436e491007d82f2d7d7074bfcfc27af
ZCRAFT <1149461164@qq.com>
2025-11-10 22:26:24 +0800

    fix: vc

--- common/src/main/java/org/craftamethyst/tritium/mixin/client/renderer/fbo/FastBlit.java
+++ common/src/main/java/org/craftamethyst/tritium/mixin/client/renderer/fbo/FastBlit.java
@@ -3,15 +3,12 @@ package org.craftamethyst.tritium.mixin.client.renderer.fbo;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.platform.GlStateManager;
 import me.zcraft.tritiumconfig.config.TritiumConfig;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import org.lwjgl.opengl.GL30;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@OnlyIn(Dist.CLIENT)
 @Mixin(RenderTarget.class)
 public abstract class FastBlit {
 

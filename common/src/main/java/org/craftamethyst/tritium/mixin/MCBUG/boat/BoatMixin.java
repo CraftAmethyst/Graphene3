bd0c00a94be837655e71af257b250f90b11b3dc6
ZCRAFT <1149461164@qq.com>
2025-11-15 18:53:21 +0800

    refactor(mixin): rename MixinBoat to BoatMixin
    - Renamed MixinBoat class to BoatMixin for consistency
    - Updated mixin configuration file to reflect the class name change

--- common/src/main/java/org/craftamethyst/tritium/mixin/MCBUG/boat/BoatMixin.java
+++ common/src/main/java/org/craftamethyst/tritium/mixin/MCBUG/boat/BoatMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Boat.class)
-public abstract class MixinBoat {
+public abstract class BoatMixin {
 
     @Inject(
             method = "checkFallDamage",

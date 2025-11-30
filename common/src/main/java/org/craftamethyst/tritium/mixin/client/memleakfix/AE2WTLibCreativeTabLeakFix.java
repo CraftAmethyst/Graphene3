3b7fba9156ac801eb4e849d85b899934a026463b
yiyuyan <179544143@qq.com>
2025-11-30 18:24:46 +0800

    Fix AE2WTLibCreativeTabLeakFix

--- common/src/main/java/org/craftamethyst/tritium/mixin/client/memleakfix/AE2WTLibCreativeTabLeakFix.java
+++ common/src/main/java/org/craftamethyst/tritium/mixin/client/memleakfix/AE2WTLibCreativeTabLeakFix.java
@@ -1,6 +1,7 @@
 package org.craftamethyst.tritium.mixin.client.memleakfix;
 
 import de.mari_023.ae2wtlib.AE2WTLibCreativeTab;
+import net.minecraft.core.Registry;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import org.craftamethyst.tritium.config.TritiumConfigBase;
@@ -9,7 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
@@ -21,7 +22,7 @@ public class AE2WTLibCreativeTabLeakFix {
     private static List<ItemStack> items;
 
     @Inject(method = "init", at = @At("HEAD"))
-    private static void tritium$clearOnInit(CallbackInfoReturnable<CreativeModeTab> ci) {
+    private static void tritium$clearOnInit(Registry<CreativeModeTab> registry, CallbackInfo ci) {
         if (TritiumConfigBase.Fixes.MemoryLeakFix.AE2WTLibCreativeTabLeakFix) {
             synchronized (items) {
                 items.clear();
